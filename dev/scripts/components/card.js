65accc4d39c63dce3bd96015e962cec4c6ac6d5f
Angel Alagao <angelalagao@gmail.com>
2017-06-29 14:58:18 -0400

    added some carousel code

--- dev/scripts/components/card.js
+++ dev/scripts/components/card.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {toggleClass} from 'jquery';
 import {
 	BrowserRouter as Router,
 	Link,
@@ -8,34 +9,72 @@ import SimpleMap from './map.js';
 
 
 export default class Card extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			cards: [],
+			currentCard: 0
+		}
+		this.handleClickLeft = this.handleClickLeft.bind(this);
+		this.handleClickRight = this.handleClickRight.bind(this);
+		this.getCards = this.getCards.bind(this);
+	}
+	getCards() {
+
+	}
+	handleClickLeft() {
+		if (this.state.currentCard === 0) {
+			this.setState({
+				currentCard: this.getCards().length - 1
+			});
+		} else {
+			this.setState({
+				currentCard: this.state.currentCard - 1
+			});
+		}
+	}
+	handleClickRight() {
+		if (this.state.currentCard === this.state.cards.length) {
+			this.setState({
+				currentCard: 0
+			});
+		} else {
+			this.setState({
+				currentCard: this.state.currentCard + 1
+			});
+		}
+	}
 	render() {
 		return (
-			<div>
+			<div className="carousel">
+				<i onClick={this.handleClickLeft}
+					className="fa fa-chevron-circle-left left-arrow" aria-hidden="true"></i>
 				{this.props.about.map((school) => {
 					return (
-						<div id={`card-${school.id}`} key={`card-${school.id}`}>
-							{/*<Carousel>*/}
-							<Link to={`/${school.slug}`}>
-								<div className="school__card">
-									<SimpleMap 
-										lat={school.latitude}
-										lng={school.longitude}
-										boundaries={school.boundaries}
-										i={`map-${school.id}`} 
-									/>
-									<h3>{school.name}</h3>
-									<h4>{school.level}</h4>
-									<p>{school.type} | {school.language}</p>
-									<div className="keyline"></div>
-								</div>
-							</Link>
-							{/*</Carousel>*/}
-						</div>
+						<Link to={`/${school.slug}`} id={`card-${school.id}`} key={`card-${school.id}`}>
+							<div className="school__card">
+								<SimpleMap 
+									lat={school.latitude}
+									lng={school.longitude}
+									boundaries={school.boundaries}
+									i={`map-${school.id}`} 
+								/>
+								<h3>{school.name}</h3>
+								<h4>{school.level}</h4>
+								<p>{school.type} | {school.language}</p>
+								<div className="keyline"></div>
+							</div>
+						</Link>
 					)
 				})}
+				<i onClick={this.handleClickRight}
+					className="fa fa-chevron-circle-right right-arrow" aria-hidden="true"></i>
 				<Route exact path="/:school" component={SchoolName} />
 			</div>
 		)
+	}
+	componentDidMount() {
+
 	}
 }
 
